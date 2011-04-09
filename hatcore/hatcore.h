8cc8fea43f47be4201f155aa9fb256504805ef20
Maoxu Li <lim@gwmail.gwu.edu>
2011-04-09 16:34:38 -0400

    Hat core lib

---
+++ hatcore/hatcore.h
@@ -0,0 +1,23 @@
+/*
+ *  hatcore.h
+ *  hatcore
+ *
+ *  Created by Maoxu Li on 4/9/11.
+ *  Copyright 2011 GWU. All rights reserved.
+ *
+ */
+
+#ifndef hatcore_
+#define hatcore_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class hatcore
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ hatcore/hatcorePriv.h
@@ -0,0 +1,19 @@
+/*
+ *  hatcorePriv.h
+ *  hatcore
+ *
+ *  Created by Maoxu Li on 4/9/11.
+ *  Copyright 2011 GWU. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class hatcorePriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
